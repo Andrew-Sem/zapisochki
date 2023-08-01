07bafed18266163fee2d4247e385a0dcbb8b62a9
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-08-01 17:27:16 +0300

    update env

--- env.mjs
+++ env.mjs
@@ -14,8 +14,8 @@ export const env = createEnv({
 		DATABASE_PASSWORD: z.string().min(1),
 		NEXTAUTH_URL: z.string().min(1),
 		NEXTAUTH_SECRET: z.string().min(1),
-		GITHUB_ID: z.string().min(1),
-		GITHUB_SECRET: z.string().min(1),
+		GITHUB_CLIENT_ID: z.string().min(1),
+		GITHUB_CLIENT_SECRET: z.string().min(1),
 	},
 
 	/**
@@ -40,8 +40,8 @@ export const env = createEnv({
 		DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		GITHUB_ID: process.env.GITHUB_ID,
-		GITHUB_SECRET: process.env.GITHUB_SECRET,
+		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+		GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
 
 		// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 	},
