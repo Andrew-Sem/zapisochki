d8e0f45a7dbe58439047c8fe413d53568a42e6c1
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-08-02 11:35:58 +0300

    link to homepage from logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { getServerSession } from 'next-auth';
 import { HeaderMenu } from './header-menu';
+import Link from 'next/link';
 
 export const Header: FC = async () => {
 	const user = await getServerSession();
@@ -8,9 +9,11 @@ export const Header: FC = async () => {
 		<header className=' sticky top-0 border-b bg-background/50 backdrop-blur-sm z-10'>
 			<div className='container flex justify-between items-center h-16'>
 				<div className='relative'>
-					<h2 className='font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-muted-foreground to-accent-foreground'>
-						Zapisochki
-					</h2>
+					<Link href={'/'}>
+						<h2 className='font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-muted-foreground to-accent-foreground'>
+							Zapisochki
+						</h2>
+					</Link>
 					<div className='absolute bg-secondary rounded-full py-1 px-2 bottom-1/3 left-full ml-1 text-xs text-sky-500'>
 						preview
 					</div>
