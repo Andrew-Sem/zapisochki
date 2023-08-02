06d1fed9278700995f81d4cdcf766d287dafc965
Andrew Semyonov <101043571+Andrew-Sem@users.noreply.github.com>
2023-08-03 00:17:23 +0300

    migrate to prisma

--- env.mjs
+++ env.mjs
@@ -8,10 +8,7 @@ export const env = createEnv({
 	 */
 	server: {
 		NODE_ENV: z.enum(['development', 'test', 'production']),
-		DATABASE_HOST: z.string().min(1),
 		DATABASE_URL: z.string().url().min(1),
-		DATABASE_USERNAME: z.string().min(1),
-		DATABASE_PASSWORD: z.string().min(1),
 		NEXTAUTH_URL: z.string().min(1),
 		NEXTAUTH_SECRET: z.string().min(1),
 		GITHUB_CLIENT_ID: z.string().min(1),
@@ -36,10 +33,7 @@ export const env = createEnv({
 	runtimeEnv: {
 		NODE_ENV: process.env.NODE_ENV,
 		NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-		DATABASE_HOST: process.env.DATABASE_HOST,
 		DATABASE_URL: process.env.DATABASE_URL,
-		DATABASE_USERNAME: process.env.DATABASE_USERNAME,
-		DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
